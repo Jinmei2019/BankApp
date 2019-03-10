d000f366c9fd18367e37dd4f8a3181e2b0f477e9
lijinmei <lijinmei@live.com>
2019-03-10 10:47:13 -0700

    Initial comment

---
+++ bankapp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace bankapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
